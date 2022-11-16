cf71848469e5a65a077ca1ad9988e7f0cc38ffa3
Pumpkinjam <95205494+Pumpkinjam@users.noreply.github.com>
2022-11-16 15:17:10 +0900

    Update GameManager.py

--- pkjgame/GameManager.py
+++ pkjgame/GameManager.py
@@ -1,5 +1,5 @@
 class GameManager:
-
+    
     def __init__(self, fps, screen_width, screen_height):
         self.fps = fps
         self.spf = 1/fps
@@ -7,6 +7,18 @@ class GameManager:
         self.screen_height = screen_height
         self.object_id = 0
 
+
+    def start(self):
+        self.setup()
+        try:
+            self.manage()
+        except:
+            return
+
+    def stop(self):
+        raise GameManager.GameEndException('Game Stopped.')
+
+    def setup(self):
         self.objects = dict()       # keys: id (int), values: obj (GameObject)
         self.am = AlarmManager()
         self.dm = DisplayManager()
@@ -14,11 +26,18 @@ class GameManager:
 
         self.dm.set_background((255, 255, 255, 100))
         self.fps_alarm = self.am.new_alarm(self.spf)
-
-        self.setup()
-        self.manage()
-
         
+        self.create(Player, (60, 60, 32, 32, DisplayManager.get_rectangle_image(32, 32, (0,0,0,100))))
+        self.dm.refresh(self.objects.values())
+        pass
+
+    def manage(self):
+        while True:
+            if self.fps_alarm.resetAlarm():
+                self.dm.refresh(self.objects.values())
+
+
+    
     def create(self, cls: type, args: tuple):
         self.object_id += 1
         self.objects[self.object_id] = cls(self.object_id, *args)
@@ -26,14 +45,10 @@ class GameManager:
     def destroy(self, id):
         del self.objects[id]
 
-    def setup(self):
-        self.create(Player, (60, 60, 32, 32, DisplayManager.get_rectangle_image(32, 32, (0,0,0,100))))
-        self.dm.refresh(self.objects.values())
-        pass
-    
 
-    def manage(self):
-        while True:
-            if self.fps_alarm.resetAlarm():
-                self.dm.refresh(self.objects.values())
-            
+
+    class GameEndException(Exception):
+        def __init__(self, msg=''):
+            print('Game Ended' if msg == '' else msg)
+            super().__init__(msg)
+        
